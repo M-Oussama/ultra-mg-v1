cfcf3feb28fb97f37120843b0040f73510bcdd96
Mahgoun Oussama <36973924+M-Oussama@users.noreply.github.com>
2024-07-25 19:39:48 +0100

    [BACK][UPD]

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -238,7 +238,7 @@ class ClientController extends Controller
 
 
         $pdf = Pdf::loadView('client_log_pdf', compact('client', 'logEntries'));
-
+        $pdf->save(storage_path().'client_log_pdf.pdf');
         return $pdf->download('client_log.pdf');
     }
 }
